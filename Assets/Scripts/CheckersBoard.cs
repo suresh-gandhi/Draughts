3206a3a1bf93f747cdb2e197cb4e974e62b9e131
suresh-gandhi <sureshgandhi200@gmail.com>
2018-05-23 23:27:14 +0530

    Everything done except the king move

--- Assets/Scripts/CheckersBoard.cs
+++ Assets/Scripts/CheckersBoard.cs
@@ -11,7 +11,12 @@ public class CheckersBoard : MonoBehaviour {
     public Vector3 boardOffset = new Vector3(-4.0f, 0, -4.0f);
     private Vector3 pieceOffset = new Vector3(0.5f, 0, 0.5f);
 
+    public bool isWhite;
+    private bool isWhiteTurn;
+    private bool hasKilled;
+
     private Piece selectedPiece;
+    private List<Piece> forcedPieces;
 
     private Vector2 mouseOver;
     private Vector2 startDrag;
@@ -19,10 +24,13 @@ public class CheckersBoard : MonoBehaviour {
 
     private void Start() {
         GenerateBoard();
+        forcedPieces = new List<Piece>();
+        isWhiteTurn = true;
     }
 
     private void Update() {
-        UpdateMouseOver();
+        Debug.Log(forcedPieces);
+        UpdateMouseOver();                      // Refreshing the mouse over in every frame
         // Debug.Log(mouseOver);            
 
         // If its my turn
@@ -30,16 +38,21 @@ public class CheckersBoard : MonoBehaviour {
             int x = (int)mouseOver.x;           // This is because in a vector they were transferred back to floats
             int z = (int)mouseOver.y;
 
+            if (selectedPiece != null) {
+                UpdatePieceDrag(selectedPiece);     // Refreshing the update drag when we have some piece selected
+            }
+
             if (Input.GetMouseButtonDown(0)) {
                 SelectPiece(x, z);
             }
 
             if (Input.GetMouseButtonUp(0)) {
-
+                TryMove((int)startDrag.x, (int)startDrag.y, x, z);
             }
         }
     }
 
+    // This function basically assigns the mouseOver correctly to the pointed block. mouseOver is a Vector2
     private void UpdateMouseOver() {
         // If its my turn
         if (!Camera.main) {
@@ -60,18 +73,150 @@ public class CheckersBoard : MonoBehaviour {
 
     }
 
+    // This function takes a piece and elevates it as we hover a mouse. However it assumes that the piece p is not null which implies a piece should be selected.
+    private void UpdatePieceDrag(Piece p) {
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            p.transform.position = hit.point + Vector3.up;
+        }
+    }
+
+    // This function selects the piece at the block (x, z) if its in the valid range else returns and does nothing. It also updates the start drag.
     private void SelectPiece(int x, int z) {
         // Out of bounds
-        if (x < 0 || x >= pieces.Length || z < 0 || z >= pieces.Length) {
+        if (x < 0 || x >= 8 || z < 0 || z >= 8) {
             return;
         }
 
         Piece p = pieces[x, z];
-        if (p != null) {
+        if (p != null && p.isWhite == isWhite) {
+
+            if (forcedPieces.Count == 0)
+            {
+                selectedPiece = p;
+                startDrag = mouseOver;
+            }
+            else {
+                if (forcedPieces.Find(fp => fp == p) == null)
+                {
+                    return;
+                }
+                selectedPiece = p;
+                startDrag = mouseOver;
+            }
+
+            /*
             selectedPiece = p;
             startDrag = mouseOver;
             Debug.Log(selectedPiece.name);
+            */
         }
+    }
+
+    private void TryMove(int x1, int y1, int x2, int y2) {
+
+        forcedPieces = ScanForPossibleMoves();
+
+        // Multiplayer Support
+        startDrag = new Vector2(x1, y1);
+        endDrag = new Vector2(x2, y2);
+        selectedPiece = pieces[x1, y1];
+
+        // MovePiece(selectedPiece, x2, y2);
+
+        // Out of bounds
+        if (x2 < 0 || x2 >= 8 || y2 < 0 || y2 >= 8) {
+            if(selectedPiece != null) {
+                MovePiece(selectedPiece, x1, y1);
+            }
+            startDrag = Vector2.zero;
+            selectedPiece = null;
+            return;
+        }
+
+        if (selectedPiece != null) {
+            // If it has not moved
+            if (endDrag == startDrag) {
+                MovePiece(selectedPiece, x1, y1);
+                startDrag = Vector2.zero;
+                selectedPiece = null;
+                return;
+            }
+
+            // Check if it is a valid move  
+            if (selectedPiece.ValidMove(pieces, x1, y1, x2, y2))
+            {
+                // Did we kill something
+                // If this is a jump
+                if (Mathf.Abs(x1 - x2) == 2)
+                {
+                    Piece p = pieces[(x1 + x2) / 2, (y1 + y2) / 2];
+                    if (p != null)
+                    {
+                        pieces[(x1 + x2) / 2, (y1 + y2) / 2] = null;
+                        Destroy(p.gameObject);
+                        hasKilled = true;
+                    }
+                }
+
+                // Were we supposed to kill anything
+                if(forcedPieces.Count != 0 && !hasKilled) {
+                    MovePiece(selectedPiece, x1, y1);
+                    startDrag = Vector2.zero;
+                    selectedPiece = null;
+                    return;
+                }
+
+                pieces[x2, y2] = selectedPiece;
+                pieces[x1, y1] = null;
+                MovePiece(selectedPiece, x2, y2);
+
+                EndTurn();
+            }
+            else {
+                MovePiece(selectedPiece, x1, y1);
+                startDrag = Vector2.zero;
+                selectedPiece = null;
+                return;
+            }
+        }
+        
+    }
+
+    private void EndTurn() {
+        selectedPiece = null;
+        startDrag = Vector2.zero;
+        isWhiteTurn = !isWhiteTurn;
+        hasKilled = false;
+        CheckVictory();
+    }
+
+    private bool CheckVictory() {
+        return false;
+    }
+
+    private List<Piece> ScanForPossibleMoves() {
+        forcedPieces = new List<Piece>();
+
+        // Check all the pieces
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (pieces[i, j] != null && pieces[i, j].isWhite == isWhiteTurn) {
+                    if (pieces[i, j].IsForceToMove(pieces, i, j)) {
+                        forcedPieces.Add(pieces[i, j]);
+                    }
+                }
+            }
+        }
+
+        return forcedPieces;
     }
 
     private void GenerateBoard() {
@@ -106,6 +251,7 @@ public class CheckersBoard : MonoBehaviour {
         MovePiece(p, x, z);
     }
 
+    // This function takes a piece p and moves it to the block (x, z)
     private void MovePiece(Piece p, int x, int z) {
         p.transform.position = (Vector3.right * x) + (Vector3.forward * z) + boardOffset + pieceOffset;
     }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -4,13 +4,124 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool isWhite;
+    public bool isKing;
+
+    public bool IsForceToMove(Piece[,] board, int x, int y)
+    {
+        if (isWhite || isKing)
+        {
+            // Top Left
+            if (x >= 2 && y <= 5)
+            {
+                Piece p = board[x - 1, y + 1];
+                // If there is a piece, and it is not the same color as ours 
+                if (p != null && p.isWhite != isWhite)
+                {
+                    // Check if it is possible to land after the jump
+                    if (board[x - 2, y + 2] == null)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            // Top Right
+            if (x <= 5 && y <= 5)
+            {
+                Piece p = board[x + 1, y + 1];
+                // If there is a piece, and it is not the same color as ours 
+                if (p != null && p.isWhite != isWhite)
+                {
+                    // Check if it is possible to land after the jump
+                    if (board[x + 2, y + 2] == null)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        else if(!isWhite || isKing){
+            // Bottom Left
+            if (x >= 2 && y >= 2)
+            {
+                Piece p = board[x - 1, y - 1];
+                // If there is a piece, and it is not the same color as ours 
+                if (p != null && p.isWhite != isWhite)
+                {
+                    // Check if it is possible to land after the jump
+                    if (board[x - 2, y - 2] == null)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            // Bottom Right
+            if (x <= 5 && y >= 2)
+            {
+                Piece p = board[x + 1, y - 1];
+                // If there is a piece, and it is not the same color as ours 
+                if (p != null && p.isWhite != isWhite)
+                {
+                    // Check if it is possible to land after the jump
+                    if (board[x + 2, y - 2] == null)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public bool ValidMove(Piece[,] board, int x1, int y1, int x2, int y2) {
+        // If you are moving on top of another piece
+        if (board[x2, y2] != null) {
+            return false;
+        }
+
+        int deltaMoveX = Mathf.Abs(x1 - x2);
+        int deltaMoveY = y2 - y1;
+
+        if (isWhite || isKing) {
+            if (deltaMoveX == 1) {
+                if(deltaMoveY == 1) {
+                    return true;
+                }
+            }
+            else if (deltaMoveX == 2) {
+                if (deltaMoveY == 2) {
+                    Piece p = board[(x1 + x2) / 2, (y1 + y2) / 2];
+                    if (p != null || p.isWhite != isWhite) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        if (!isWhite || isKing)
+        {
+            if (deltaMoveX == 1)
+            {
+                if (deltaMoveY == -1)
+                {
+                    return true;
+                }
+            }
+            else if (deltaMoveX == 2)
+            {
+                if (deltaMoveY == -2)
+                {
+                    Piece p = board[(x1 + x2) / 2, (y1 + y2) / 2];
+                    if (p != null || p.isWhite != isWhite)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
 }
